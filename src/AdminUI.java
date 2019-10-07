a432ae52c5d44585e6da754398042a477e679c7c
fgomez <fgvolonterio@gmail.com>
2019-10-07 20:59:27 +0000

    Funciona el selector de tablas (Para ver los campos). Retoques al login. La logica de la UI es criminal.

--- src/AdminUI.java
+++ src/AdminUI.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.sql.*;
 
 public class AdminUI extends JPanel {
@@ -14,6 +18,7 @@ public class AdminUI extends JPanel {
 
     public AdminUI(JFrame ventana, Connection conexion) {
 
+        String[] initData = {"cad1", "cad2", "cad3"};
         try {
             this.conexion = conexion;
             this.ventana = ventana;
@@ -21,13 +26,17 @@ public class AdminUI extends JPanel {
             sentencias = new JTextField();
             resultados = new JTable();
 
-            tablas = new JList<>(armarTablas().split(" "));
-            campos = new JList<>();
+            tablas = new JList<>(obtenerTablas());
+            campos = new JList<>(initData);
+
+            sentencias.addKeyListener(new EnterListener());
+            tablas.addListSelectionListener(new SeleccionListener(this));
 
             this.setLayout(new BorderLayout());
             this.add(sentencias, BorderLayout.NORTH);
             this.add(resultados, BorderLayout.CENTER);
             this.add(tablas, BorderLayout.WEST);
+            this.add(campos, BorderLayout.SOUTH);
 
             ventana.getContentPane().add(this);
         } catch (SQLException e) {
@@ -35,16 +44,86 @@ public class AdminUI extends JPanel {
         }
     }
 
-    private String armarTablas() throws SQLException {
-        String tablas = "Test string";
+    private String[] obtenerTablas() throws SQLException {
+        String tablas[] = new String[15];
         DatabaseMetaData db = conexion.getMetaData();
         ResultSet rs;
 
         rs = db.getTables(null, null, "%", null);
-        while (rs.next())
-            tablas = rs.getString(3);
+        int k = 0;
+        while (rs.next()) {
+            tablas[k++] = rs.getString(3);
+        }
         rs.close();
 
         return tablas;
+    }
+
+    private void ejecutarSentencia(String sent) {
+        try {
+            String comando = sent.split(" ")[0];
+            ResultSet resultado;
+            Statement sentencia = conexion.createStatement();
+
+            if (!sentencia.execute(sent));
+
+                System.out.println(comando);
+            // Si la sentencia retorna valor, lo recuperamos y luego cerramos
+            if (!comando.toLowerCase().equals("insert") &&
+                    !comando.toLowerCase().equals("delete") &&
+                    !comando.toLowerCase().equals("update")) {
+                resultado = sentencia.getResultSet();
+
+                int k = 0;
+                while (resultado.next()) {
+                    System.out.println(resultado.getString(k++));
+                }
+            }
+
+            sentencia.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class EnterListener implements KeyListener {
+
+        public void keyTyped(KeyEvent keyEvent) {}
+
+        public void keyPressed(KeyEvent keyEvent) {
+            if (keyEvent.getKeyChar() == '\n')
+                ejecutarSentencia(sentencias.getText());
+        }
+
+        public void keyReleased(KeyEvent keyEvent) {}
+    }
+
+    private class SeleccionListener implements ListSelectionListener {
+        protected JPanel panel;
+        public SeleccionListener(AdminUI adminUI) {
+            panel = adminUI;
+        }
+
+        @Override
+        public void valueChanged(ListSelectionEvent listSelectionEvent) {
+            try {
+                int i = 1;
+                String tabla = tablas.getSelectedValue();
+                String[] camposTabla;
+                Statement sentencia = conexion.createStatement();
+                ResultSet rs = sentencia.executeQuery("describe " + tabla);
+
+                camposTabla = new String[21];
+                while (rs.next()) {
+                    camposTabla[i] = rs.getString("Field");
+                    i++;
+                }
+
+                campos.removeSelectionInterval(0, i-1);
+                campos.setListData(camposTabla);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
